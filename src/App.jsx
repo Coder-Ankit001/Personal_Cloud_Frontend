ab364099b798ce5b9b6921b039fe924edf0cb2ff
Ankit Chaudhary <ankitchaudhary7157@gmail.com>
2026-06-19 12:02:11 +0530

    Project Initialized

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+    <div className="h-screen w-screen flex justify-center items-center bg-slate-800 text-white">
+        <div className="text-3xl font-semibold">Getting started with new project!</div>
+    </div>
+    </>
+  );
+}
+
+export default App;
