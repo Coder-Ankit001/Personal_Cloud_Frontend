42b3b4f392a4a9bb103f9c041eef92ad6a230f59
Ankit Chaudhary <ankitchaudhary7157@gmail.com>
2026-06-26 09:44:58 +0530

    Feature: Added download file handler using API

--- src/components/NodeOptions.jsx
+++ src/components/NodeOptions.jsx
@@ -1,21 +1,69 @@
+import axios from 'axios'
+import { useAuth } from '../hooks/useAuth';
 import { useFileSystem } from '../hooks/useFileSystem'
 
 import { NODE_OPTIONS } from "./ui/FileSystem"
+import { useState } from 'react';
+
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
 
 const NodeOptions = ({ type }) => {
-
+  const { accessToken } = useAuth()
   const { setNodeForm, selectNode } = useFileSystem()
+  const [error, setError] = useState()
 
+  const handleDownloadFile = async() => {
+    console.log("Download triggered")
+    if((!selectNode)) return
+    console.log(`${BACKEND_URL}/storage/download/${selectNode}`)
+    try{
+      const res = await axios.get(
+        `${BACKEND_URL}/storage/download/${selectNode}`, 
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`
+          },
+          withCredentials: true, 
+          responseType: 'blob',
+        })
+
+      const fileName = res.headers['x-filename'] || 'downloaded-file'
+      
+      // Convert binary response into temporary browser URL
+      const blobUrl = window.URL.createObjectURL(res.data)
+      const link = document.createElement('a')
+      link.href = blobUrl
+
+      // Assign file name for file download prompt
+      // Inject link into DOM, and virtually click it
+      link.setAttribute('download', fileName || 'download-file')
+      document.body.appendChild(link)
+      link.click()
+
+      // Free up memory
+      document.body.removeChild(link);
+      window.URL.revokeObjectURL(blobUrl)
+    }
+    catch(e){
+      console.error("Download Fail", e)
+      setError(e)
+    }
+  }
 
   return (
-    <div className={`absolute bottom-12  ${type === 'folder'} ?  bg-slate-900/80 z-50 w-44 border border-white/10 rounded-xl shadow-xl`}>
+    <div className={`absolute bottom-12 left-0 ${type === 'folder'} ?  bg-slate-900/80 z-50 w-44 border border-white/10 rounded-xl shadow-xl`}>
     {NODE_OPTIONS
-        .filter((opt) => opt.nodeType === 'both' || opt.nodeType === type)
+        .filter((opt) => opt.nodeType === 'BOTH' || opt.nodeType === type)
         .map((opt) => (
         <div
             key={opt.action}
             onClick={()=>{
-              opt.label === 'Rename' && setNodeForm({id: selectNode, type})
+              if(opt.label === 'Download' && opt.nodeType === 'FILE'){
+                handleDownloadFile()
+              }
+              else if(opt.label === 'Rename'){
+                setNodeForm({id: selectNode, type})
+              }
             }}
             className={`flex items-center gap-2.5 w-full px-3.5 py-2 text-sm transition-colors hover:bg-white/5
             ${opt.danger ? 'text-rose-400' : 'text-slate-300'}`}
