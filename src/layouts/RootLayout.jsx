527ae7f8ad2d9d9cb5f836d36a810d12cdbf6e1d
Ankit Chaudhary <ankitchaudhary7157@gmail.com>
2026-06-29 15:29:43 +0530

    Fix: Resolved file system context import causing error due to case-sensitive in windows

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router'
 
 import { AuthProvider } from '../contexts/AuthContext'
-import { FileSystemProvider } from '../contexts/FIleSystemContext'
+import { FileSystemProvider } from '../contexts/FileSystemContext'
 
 const RootLayout = () => {
   return (
